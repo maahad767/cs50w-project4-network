704b1c6efd610010efc422f5ff28f2e82d123d5e
Abdul Ahad <abdul.ahad@brainstation-23.com>
2023-09-03 20:33:42 +0600

    added edit post, like and unlike post urls

--- network/urls.py
+++ network/urls.py
@@ -10,8 +10,10 @@ urlpatterns = [
     path("posts/all", views.all_posts, name="all-posts"),  
     path("posts/following", views.following_posts, name="following-posts"),  
     path("posts/create", views.create_post, name="create-post"),  
+    path("posts/<int:post_id>/edit", views.edit_post, name="edit-post"),  
     path("profile/<str:username>", views.profile, name="profile"),  
-    
+    path("posts/<int:post_id>/like", views.like_post, name="like-post"),  
+    path("posts/<int:post_id>/unlike", views.unlike_post, name="unlike-post"),  
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
