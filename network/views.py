d6356874d3ae6ac519e8caedbccf88258d004905
Abdul Ahad <abdul.ahad@brainstation-23.com>
2023-09-04 12:12:32 +0600

    added profile pages and follow/unfollow views

--- network/views.py
+++ network/views.py
@@ -3,17 +3,21 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.db.models import Q 
-from django.http import HttpResponseRedirect, JsonResponse
+from django.http import HttpResponseBadRequest, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import Post, User, Like
+from .models import Follow, Post, User, Like
 
 
 def index(request):
+    user = request.user
+    if not request.user.is_authenticated:
+        user = None
     all_posts = Post.objects.all().annotate(
-        is_liked=Q(likes__user=request.user)
+        is_liked=Q(likes__user=user)
     )
+    # TODO fix the is_liked invalid bug
     context = {
         "posts": all_posts
     }
@@ -31,14 +35,26 @@ def all_posts(request):
 @login_required
 def following_posts(request):
     # do pagination
-    posts = Post.objects.filter(author=request.user)
+    posts = Post.objects.filter(author__in=request.user.following.values("following")).annotate(
+        is_liked=Q(likes__user=request.user)
+    )
+
     return render(request, "network/following.html", {"posts": posts})
 
 
 def profile(request, username):
-    posts = Post.objects.filter(author__username=username)
+    posts = Post.objects.filter(author__username=username).annotate(
+        is_liked=Q(likes__user=request.user)
+    )
     
-    return render(request, "network/profile.html")
+    return render(request, "network/profile.html", {
+        "profile_user": User.objects.get(username=username),
+        "posts": posts,
+        "is_follower": Follow.objects.filter(
+            follower=request.user,
+            following__username=username
+        ).exists(),
+    })
 
 
 @login_required
@@ -81,6 +97,30 @@ def unlike_post(request, post_id):
     }
     return JsonResponse(data)
 
+@login_required
+def follow(request, username):
+    follow_user = User.objects.get(username=username)
+    Follow.objects.get_or_create(
+        follower=request.user,
+        following=follow_user,
+    )
+    
+    return JsonResponse({"follower_count": follow_user.followers.count()})
+
+
+@login_required
+def unfollow(request, username):
+    unfollow_user = User.objects.get(username=username)
+    try:
+        follow_obj = Follow.objects.get(
+            follower=request.user,
+            following=unfollow_user,
+        )
+        follow_obj.delete()
+    except:
+        print("Not Followed")
+        return HttpResponseBadRequest("Not Followed")
+    return JsonResponse({"follower_count": unfollow_user.followers.count()})
 
 def login_view(request):
     if request.method == "POST":
